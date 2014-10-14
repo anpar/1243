3fc0d00d1e402491082a1742a6a5fa8a5a1ecff4
PFair <peterfairr@gmail.com>
2014-10-14 09:47:56 +0200

    Update OutilDeGestion.m

--- projet/tache1/OutilDeGestion.m
+++ projet/tache1/OutilDeGestion.m
@@ -1,12 +1,12 @@
 function [Ar, N2, O2, H2O, CH4] = OutilDeGestion(T, NH3)
 % Groupe 124.3 - 13/10/2014 
-% Retourne les masses/jour de matières premières :
-% - Air : N2, O2, Ar, CO ;
+% Retourne les masses/jour de matiÃ¨res premiÃ¨res :
+% - Air : N2, O2, Ar;
 % - CH4, H20
-% Input : T en Kelvin et NH3 en t/d.
-% Output : matières premières en tonnes.
+% Input : T en Kelvin et NH3 en tonnes/jour.
+% Output : matiÃ¨res premiÃ¨res en tonnes.
 %
-% Sponsorisé par Coca-Cola (Ltd.) et Bandai & co.
+% SponsorisÃ© par The Coca-Cola Company. Open Happiness.
 
 MNH3 = 17; % Masse molaire du NH3
 nNH3 = (NH3*1000*1000)/MNH3; 
@@ -16,18 +16,18 @@ nNH3 = (NH3*1000*1000)/MNH3;
 R = 8.3144621; % Constante des gaz parfaits
 
 % ----------------------------------------------------------
-%      Constantes d'équilibre (c) by Simon Quiriny
+%      Constantes d'Ã©quilibre (c) by Simon Quiriny
 % ----------------------------------------------------------
 G1 = (188369.9) - (71.16*T*log(T)) + (238.21*T) + (0.04163*T^2) - ((4.045e-6)*T^3);
 K1 = exp(-G1/(R*T)); 
 G2 = (-42533.33) + (69.67*T) - (2.93e-3)*(T^2) + (2.1e-7)*(T^3) - (3.77*T*log(T));
 K2 = exp(-G2/(R*T));
-% ptot = 28bars, pression à la sortie du réformage primaire
+% ptot = 28bars, pression Ã  la sortie du rÃ©formage primaire
 ptot = 28e5;
 % p0 = pression standard
 p0 = 1e5;
 
-% On montre à MatLab quelles sont les variables du système et on lui dit de
+% On montre Ã  MatLab quelles sont les variables du systÃ¨me et on lui dit de
 % choisir les valeurs positives uniquement.
 syms x y H2O CH4 positive;
 eqn1 = K1 == ((x-y)*((3*x + y)^3)*ptot^2)/((H2O-x)*(H2O-x-y)*(H2O+CH4+(2*x))*p0^2);
@@ -36,10 +36,10 @@ eqn3 = CH4 - x == 7/442 * nNH3;
 eqn4 = 3*x + y == (9/221 * nNH3) -x+y;
 eqns = [eqn1 eqn2 eqn3 eqn4];
 
-% Solutions du système en moles
+% Solutions du systÃ¨me en moles
 [x, y, H2O, CH4] = solve(eqns);  
 
-% On va remettre les solutions du système en tonnes
+% On va remettre les solutions du systÃ¨me en tonnes
 MAr = 39.948;
 MN2 = 28.014;
 MO2 = 31.998;
