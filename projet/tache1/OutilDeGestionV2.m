3e9a4b404ef2cc136baf4d34c5fcfa9b755fe380
Antoine Paris <paris.antoine.paris@gmail.com>
2014-11-13 09:20:10 +0100

    Add comment about eqn4

--- projet/tache1/OutilDeGestionV2.m
+++ projet/tache1/OutilDeGestionV2.m
@@ -19,20 +19,21 @@ ptot = 26e5;
 p0 = 1e5;
 
 % Equation d'equilibre du reformage primaire
-syms x y n01 n02 positive;
+syms x y n01 n02;
 eqn1 = K1 == ((x-y)*((3*x + y)^3)*ptot^2)/((n01-x)*(n02-x-y)*(n01+n02+(2*x))^2*p0^2);
 eqn2 = K2 == (y*(3*x + y))/((x-y)*(n02-x-y));
 eqn3 = n01 - x == (0.42*a*10^6)/(26.52*86400);
-eqn4 = 3*x + y == 2*(0.42*a*10^6)/(26.52*86400) -x+y;
-% Pourquoi 9/221 ? Dans le bilan de matiere on dirait plutôt
-% que c'est 7/221
+eqn4 = 3*x + y == 2*(0.42*a*10^6)/(26.52*86400)-x+y;
+% D'ou vient l'eqn4 ?
 eqns = [eqn1 eqn2 eqn3 eqn4];
-[x, y, n01, n02] = solve(eqns); 
+[x, y, n01, n02] = solve(eqns, x, y, n01, n02); 
 x
 y
 n01
 n02
+
 % Nombre de tubes
+Tubes = 0;
 
 % Reformer primaire
 CH4_in1 = 0;
@@ -74,13 +75,13 @@ Ar_in4 = (0.01*a*10^6)/(26.52*86400);
 NH3_out =  (a*10^6)/(17*86400);
 
 % Un peu de mise en forme...
-Elements = {'REFORMER PRIMAIRE'; 'CH4 (in)1'; 'H2O (in)1'; 'FOUR'; 'CH4 (in)2'; 'O2 (in)1'; 
-    'REFORMER SECONDAIRE'; 'CH4 (in)3'; 'H2O (in)2'; 'CO (in)1'; 'CO2 (in)1'; 'H2 (in)1';
-    'O2 (in)2'; 'N2 (in)1'; 'Ar (in)1'; 'WATER-GAS-SHIFT'; 'CO (in)2'; 'CO2 (in)2'; 'N2 (in)2'
-    ; 'H2 (in)2'; 'Ar (in)2'; 'H2O (in)3'; 'SEPARATION'; 'CO2 (in) = CO2 (out)'; 'N2 (in)3';
-    'H2 (in)3'; 'Ar (in)3'; 'H2O (in) = H2O (out)'; 'AMMONIA SYNTHESIS'; 'N2 (in)4'; 
+Elements = {'-- TUBES'; '-- REFORMER PRIMAIRE'; 'CH4 (in)1'; 'H2O (in)1'; '-- FOUR'; 'CH4 (in)2'; 'O2 (in)1'; 
+    '-- REFORMER SECONDAIRE'; 'CH4 (in)3'; 'H2O (in)2'; 'CO (in)1'; 'CO2 (in)1'; 'H2 (in)1';
+    'O2 (in)2'; 'N2 (in)1'; 'Ar (in)1'; '-- WATER-GAS-SHIFT'; 'CO (in)2'; 'CO2 (in)2'; 'N2 (in)2'
+    ; 'H2 (in)2'; 'Ar (in)2'; 'H2O (in)3'; '-- SEPARATION'; 'CO2 (in) = CO2 (out)'; 'N2 (in)3';
+    'H2 (in)3'; 'Ar (in)3'; 'H2O (in) = H2O (out)'; '-- AMMONIA SYNTHESIS'; 'N2 (in)4'; 
     'H2 (in)4'; 'Ar (in) = Ar (out)'; 'NH3 (out)'};
-MolesBySecond = {'----------'; CH4_in1; H2O_in1; '----------'; CH4_in2; O2_in1; 
+MolesBySecond = {Tubes;'----------'; CH4_in1; H2O_in1; '----------'; CH4_in2; O2_in1; 
     '----------'; CH4_in3; H2O_in2; CO_in1; CO2_in1; H2_in1;
     O2_in2; N2_in1; Ar_in1; '----------'; CO_in2; CO2_in2; N2_in2
     ; H2_in2; Ar_in2; H2O_in3; '----------'; CO2_in_out; N2_in3;
